1613c2978c776b5fce43a316512e82004d648b80
Abhinav Kumar4 <abhinav_kumar4@epam.com>
2025-03-05 13:41:29 +0530

    finally

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,16 +1,2 @@
-exports.handler = async (event) => {
-    // Determine request path and method
-    const path = event.rawPath || event.path || "/";
-    const method = event.requestContext?.http?.method || event.httpMethod || "UNKNOWN";
 
-    // Handle only /hello GET request
-    if (path === "/hello" && method === "GET") {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({statusCode:200, message: "Hello from Lambda" }), // Removed duplicate statusCode
-        };
-    }
-
-
-};
 
